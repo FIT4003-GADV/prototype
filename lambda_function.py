56477343bf8869cefbb97ef8e0db63fefcfa9653
akshay <akshay.k1738@gmail.com>
2021-10-06 18:52:44 -0500

    finish lambda handler skeleton

--- lambda_function.py
+++ lambda_function.py
@@ -8,14 +8,21 @@ from base_workflow import BaseWorkflow
 logging.set_verbosity('debug')
 logging.set_stderrthreshold('debug')
 
+# This property must exist in the input event.
+MAIN_EVENT_KEY_NAME = 'svg'
+
 
 def lambda_handler(event, context):
-    logging.info(f'Received input event: {event}')
-    result = handle(event, context)
+    logging.debug(f'Received input event: {event}, with context: {context}')
+    result = handle(event)
     return result
 
 
-def handle(event, context):
-    bw = BaseWorkflow('')
-    print(type(event), type(context))
-    return 2
+def handle(event):
+    if MAIN_EVENT_KEY_NAME not in event:
+        raise ValueError('Failed. "svg" property must be supplied in event.')
+    try:
+        bw = BaseWorkflow(event['svg'])
+        return bw.execute()
+    except Exception as ex:
+        raise ValueError(f'Failed to complete workflow, error: {ex}')
