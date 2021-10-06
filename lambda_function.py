cfdb952cba2d36d0cebe59b500942d80d6e2cdaa
akshay <akshay.k1738@gmail.com>
2021-10-06 17:48:04 -0500

    Import workflow into lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,11 @@ Entry-point to the AWS Lambda handler.
 """
 from absl import logging
 
+from base_workflow import BaseWorkflow
+
+logging.set_verbosity('debug')
+logging.set_stderrthreshold('debug')
+
 
 def lambda_handler(event, context):
     logging.info(event)
@@ -12,5 +17,6 @@ def lambda_handler(event, context):
 
 
 def handle(event, context):
+    bw = BaseWorkflow('')
     print(type(event), type(context))
     return 2
