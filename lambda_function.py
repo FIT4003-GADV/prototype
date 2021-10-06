1a8d9470bfbcb1fbcef69ab7c9b0e28f59df6834
akshay <akshay.k1738@gmail.com>
2021-10-06 17:41:21 -0500

    Update logging in lambda handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,16 @@
 """
 Entry-point to the AWS Lambda handler.
 """
+from absl import logging
 
 
 def lambda_handler(event, context):
-    print(event, context)
-    return 'Hello there...'
+    logging.info(event)
+    logging.info(context)
+    result = handle(event, context)
+    return result
+
+
+def handle(event, context):
+    print(type(event), type(context))
+    return 2
