598973e72ace193c4ac09ca04aacdf82ed552b75
akshay <akshay.k1738@gmail.com>
2021-10-07 00:55:18 -0500

    update lambda handler to comply with API gateway events

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,30 @@
 """
 Entry-point to the AWS Lambda handler.
 """
+import json
+
 from absl import logging
 
 from base_workflow import BaseWorkflow
 
-logging.set_verbosity('debug')
-logging.set_stderrthreshold('debug')
+logging.set_verbosity('info')
+logging.set_stderrthreshold('info')
 
 # This property must exist in the input event.
 MAIN_EVENT_KEY_NAME = 'svg'
 
 
 def lambda_handler(event, context):
-    logging.debug(f'Received input event: {event}, with context: {context}')
-    result = handle(event)
-    return result
+    logging.debug(f'Received input event: {event}')
+    logging.info(f'Context of event: {context}')
+
+    decoded_event_body = json.loads(event['body'])
+
+    result = handle(decoded_event_body)
+    return {
+        'status': 200,
+        'result': json.dumps(result)
+    }
 
 
 def handle(event):
