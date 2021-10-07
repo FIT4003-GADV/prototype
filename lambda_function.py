f911ca5f986c196c5aca5ae1615bdecfe45d0e67
akshay <akshay.k1738@gmail.com>
2021-10-07 13:23:56 -0500

    Add additional validation to lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ Entry-point to the AWS Lambda handler.
 import json
 
 from absl import logging
+import xml.etree.ElementTree as ET
 
 from base_workflow import BaseWorkflow
 
@@ -18,6 +19,9 @@ def lambda_handler(event, context):
     logging.debug(f'Received input event: {event}')
     logging.info(f'Context of event: {context}')
 
+    if 'body' not in event:
+        raise ValueError('Failed. Missing "body" property in event.')
+
     decoded_event_body = json.loads(event['body'])
 
     result = handle(decoded_event_body)
@@ -31,7 +35,15 @@ def handle(event):
     if MAIN_EVENT_KEY_NAME not in event:
         raise ValueError('Failed. "svg" property must be supplied in event.')
     try:
+        try_parse(event['svg'])
         bw = BaseWorkflow(event['svg'])
         return bw.execute()
     except Exception as ex:
         raise ValueError(f'Failed to complete workflow, error: {ex}')
+
+
+def try_parse(svg_string):
+    try:
+        ET.fromstring(svg_string)
+    except ET.ParseError as err:
+        raise ValueError(f'Failed. Input SVG string is not in valid XML format: {err}')
